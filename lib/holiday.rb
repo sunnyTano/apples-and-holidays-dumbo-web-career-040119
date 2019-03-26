48dc326022c37b82814e07f1e7212f95e7369781
Susan Tan <ssusantan9@gmail.com>
2019-03-26 22:02:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,16 +39,22 @@ all_supplies_in_holidays(holiday_hash)
 all_supplies_in_holidays(holiday_supplies)
 end
 
+
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |holiday, season|
-    holiday.each do |holiday, supply|
-      if supply == "BBQ"
-        puts holiday_hash[season][holiday]
+  result = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |supply, item|
+    item.each do |value|
+      if value == "BBQ"
+    result << supply
 end
 end
-
+end
+end
+result
+end
 
 
 
